aed06ec604edec2c2adf3dbdf0f98a3aecd8042b
Teresa Romero <romero.teresa@protonmail.com>
2025-05-09 09:39:11 +0200

    Update PostPutRequest to allow optional fields for Type, MaxSalary, and MinSalary

--- internal/models/handlers.go
+++ internal/models/handlers.go
@@ -17,9 +17,9 @@ type PostPutRequest struct {
 	Company     string   `json:"company"`
 	Location    string   `json:"location"`
 	Description string   `json:"description"`
-	Type        JobType  `json:"type" binding:"jobtype"`
-	MaxSalary   int      `json:"max_salary" binding:"gt=0"`
-	MinSalary   int      `json:"min_salary" binding:"gt=0"`
+	Type        JobType  `json:"type" binding:"omitempty,jobtype"`
+	MaxSalary   int      `json:"max_salary" binding:"omitempty,gt=0"`
+	MinSalary   int      `json:"min_salary" binding:"omitempty,gt=0"`
 	Perks       []string `json:"perks"`
 	Extras      string   `json:"extras"`
 }
